71f705d2226edb16bdf6d684ec3e254b199409d8
Sabina Beissenbekova <beissenb@shernet.sheridancollege.ca>
2026-03-11 11:46:48 -0400

    tried to do the coding gym

--- Assets/Scripts/Controller Input.cs
+++ Assets/Scripts/Controller Input.cs
@@ -4,7 +4,7 @@ using UnityEngine.InputSystem;
 public class ControllerInput : MonoBehaviour
 {
     public float speed = 5;
-    public Vector2 movement;
+    public Vector2 movement;   
     public AudioSource SFX;
 
     void Start()
@@ -15,9 +15,9 @@ public class ControllerInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {     
-        transform.position = movement;
-
-        //transform.position += (Vector3)movement * speed * Time.deltaTime;
+        //transform.position = movement;
+        
+        transform.position += (Vector3)movement * speed * Time.deltaTime;
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -39,4 +39,9 @@ public class ControllerInput : MonoBehaviour
         //the same as Mouse.current.position.ReadValue()
         movement = Camera.main.ScreenToWorldPoint(context.ReadValue<Vector2>());
     }
+
+    //public void OnLook(InputAction.CallbackContext context)
+    //{
+    //    rotation = context.ReadValue<Vector3>();
+    //}
 }
