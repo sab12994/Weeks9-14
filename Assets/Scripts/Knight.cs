84e431f21a98dc5681b7b7f6f1bad9f12019339c
Sabina Beissenbekova <beissenb@shernet.sheridancollege.ca>
2026-03-25 10:50:32 -0400

    tried doing the coding gym, saved the scene

--- Assets/Scripts/Knight.cs
+++ Assets/Scripts/Knight.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 public class Knight : MonoBehaviour
 {
     public AudioSource SFX;
+    public Transform start;
+    public Transform end;
+    public float t;
+    public AnimationCurve curve;
+    public float x;
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,7 +19,16 @@ public class Knight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //transform.position +=(Vector3)movement * speed * Time.deltaTime;
+
+        t += Time.deltaTime;
+        if(t > 1)
+        {
+            t = 0;
+        }
+
+        transform.position = Vector2.Lerp(start.position, end.position, t); 
+        x = curve.Evaluate(t);
     }
 
     public void Footstep()
